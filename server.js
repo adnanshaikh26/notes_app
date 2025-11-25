aa1fa59a5bef46bbf71efb383ed93645192971b2
adnanshaikh26 <adnan.shk.26@gmail.com>
2025-11-25 17:58:44 +0530

    feat(routes):"implement routes"

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/status", (req, res) => {
-  res.json({ status: "OK" });
-});
-
-app.post("/", (req, res) => {
-  const data = req.body;
-  console.log("Received data:", data);
-  res.json({ message: "User received", data });
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
